c8116b2711bfacf86f157132fe6fb379c3a69c20
Logan Dam <dam.loganza@gmail.com>
2022-12-06 18:45:56 +0100

    wip

--- day20/Program.cs
+++ day20/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var lines = File.ReadAllLines("input.txt");
+List<Tile> tiles = new();
+for (var i = 0; i < lines.Length; i += 12)
+{
+    var id = int.Parse(lines[i].Replace(":", "").Split(' ')[1]);
+    var activeTile = new Tile { Id = id };
+    var start = i + 1;
+    var end = i + 10;
+    activeTile.Bits.Add(lines[start]);
+    activeTile.Bits.Add(lines[end++]);
+    activeTile.Bits.Add(string.Join("", lines[start..end].Select(x => x[0])));
+    activeTile.Bits.Add(string.Join("", lines[start..end].Select(x => x[9])));
+    tiles.Add(activeTile);
+}
+
+
+class Tile
+{
+    public int Id { get; set; }
+    public List<string> Bits { get; set; } = new();
+}
